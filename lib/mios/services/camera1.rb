a1637481efa7de7a797f985976b12627c18e4e1e
Kelley Reynolds <kelley@insidesystems.net>
2012-10-10 09:40:46 -0400

    Add methods to get camera url

--- lib/mios/services/camera1.rb
+++ lib/mios/services/camera1.rb
@@ -4,6 +4,14 @@ module MiOS
       def self.extended(base)
         base.instance_variable_set("@camera1_urn", "urn:micasaverde-com:serviceId:Camera1")
       end
+      
+      def url
+        value_for(@camera1_urn, 'URL')
+      end
+
+      def full_url
+        "http://#{self.ip}/#{url}"
+      end
     end
   end
 end
