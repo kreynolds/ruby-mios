77996ae7d8c5ea9d16b96cfd0c534b060de86678
Kelley Reynolds <kelley@insidesystems.net>
2012-10-10 09:41:28 -0400

    Add some additional methods to HaDevice

--- lib/mios/services/ha_device1.rb
+++ lib/mios/services/ha_device1.rb
@@ -5,12 +5,24 @@ module MiOS
         base.instance_variable_set("@hadevice1_urn", "urn:micasaverde-com:serviceId:HaDevice1")
       end
 
-      def battery_level
-        integer_for(@hadevice1_urn, "BatteryLevel")
+      def auto_configure
+        boolean_for(@hadevice1_urn, "AutoConfigure")
       end
 
       def battery_date
         timestamp_for(@hadevice1_urn, "BatteryDate")
+      end
+
+      def battery_level
+        integer_for(@hadevice1_urn, "BatteryLevel")
+      end
+
+      def first_configured
+        timestamp_for(@hadevice1_urn, "FirstConfigured")
+      end
+
+      def last_update
+        timestamp_for(@hadevice1_urn, "LastUpdate")
       end
     end
   end
