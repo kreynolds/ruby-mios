0a10b758391810f0c10847bedde2388a467ea380
Jason York <github@yorkspace.com>
2014-03-26 18:56:13 -0500

    Device tests

--- lib/mios/device.rb
+++ lib/mios/device.rb
@@ -20,25 +20,27 @@ module MiOS
     end
 
     def reload
-      parse(MultiJson.load(@client.get("#{@base_uri}/data_request", {:id => 'status', :DeviceNum => attributes['id'], :output_format => :json}).content)["Device_Num_#{attributes['id']}"], true)
-      
+      client_get = @client.get("#{@base_uri}/data_request", {:id => 'status', :DeviceNum => attributes['id'], :output_format => :json})
+      load_result = MultiJson.load(client_get.content)
+      parse(load_result["Device_Num_#{attributes['id']}"], true)
+
       self
     end
-    
+
     private
-    
+
     def set(urn, action, params, async=false, &block)
       MiOS::Action.new(self, urn, action, params).take(async, &block)
     end
-    
+
     def timestamp_for(urn, key)
       Time.at integer_for(urn, key)
     end
-    
+
     def boolean_for(urn, key)
       integer_for(urn, key) == 1
     end
-    
+
     def integer_for(urn, key)
       value_for(urn, key).to_i
     end
@@ -59,17 +61,17 @@ module MiOS
           end
         end
       end
-      
+
       nil
     end
-    
+
     def parse(data, skip_attributes=false)
 
       @attributes = Hash[
         data.select { |k, v|
           !data[k].kind_of?(Hash) and !data[k].kind_of?(Array)
         }.to_a
-      ] unless skip_attributes     
+      ] unless skip_attributes
       @states = data['states']
       @states.map { |state|
         state['service'].split(":").last
@@ -81,7 +83,7 @@ module MiOS
           $stderr.puts "WARNING: #{service} not yet supported"
         end
       }
-      
+
       true
     end
   end
--- spec/lib/mios/device_spec.rb
+++ spec/lib/mios/device_spec.rb
@@ -1,10 +1,62 @@
 require 'spec_helper'
 
-describe "device" do
-  it "should correctly parse service names" do
-    output = capture_stderr do
-      device = MiOS::Device.new(nil, '', MultiJson.load(File.read('spec/support/device_data/echo-energy-manager.json')))
+module MiOS
+  module Services
+    module Bar
     end
-    expect(output).to eql "WARNING: EEMPlugin1 not yet supported\n"
   end
+end
+
+describe MiOS::Device do
+  before :each do
+    states = [ { 'service' => "foo:Bar" }, { 'service' => "foo:Tar" } ]
+    data = { 'id' => '1', 'states' => states }
+    client = double(get:
+      double(content: "{\"Device_Num_1\": #{MultiJson.dump(data)}}"))
+    @output = capture_stderr do
+      @device = MiOS::Device.new(client, 2, data)
+    end
+  end
+
+  # it "should correctly parse service names" do
+  #   output = capture_stderr do
+  #     device = MiOS::Device.new(nil, '', MultiJson.load(File.read('spec/support/device_data/echo-energy-manager.json')))
+  #   end
+  #   expect(output).to eql "WARNING: EEMPlugin1 not yet supported\n"
+  # end
+
+  describe :initialize do
+    it "should be an instance of Device" do
+      expect(@device).to be_an_instance_of(MiOS::Device)
+    end
+
+    it "should output a warning for unsupported services" do
+      expect(@output).to eql "WARNING: Tar not yet supported\n"
+    end
+  end
+
+  describe :inspect do
+    it "should return a string representation of the object" do
+      expect(@device.inspect).to eql "#<MiOS::Device:0x#{'%x' % (@device.object_id << 1)} name=>"
+    end
+  end
+
+  describe :method_missing do
+    it "should throw an exception for missing attributes" do
+      expect {@device.foo}.to raise_error
+    end
+
+    it "should not throw an exception for defined attributes" do
+      @device.attributes['id'] = 1
+      expect {@device.id}.to_not raise_error
+      expect(@device.id).to eql 1
+    end
+  end
+
+  describe :reload do
+    it "should return itself" do
+      expect(@device.reload).to eql @device
+    end
+  end
+
 end
